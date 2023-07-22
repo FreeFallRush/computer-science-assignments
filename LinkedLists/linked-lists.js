050cafbc32baa272bac5ea29216c2182c32a9f55
FreeFallRush <queenofcups.altbot@gmail.com>
2023-07-23 01:32:34 +0300

    created LinkedLists class with append(value)

--- LinkedLists/linked-lists.js
+++ LinkedLists/linked-lists.js
@@ -9,3 +9,25 @@ class Node {
     this.nextNode = nextNode;
   }
 }
+
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+    this.tailNode = null;
+    this._size = 0;
+  }
+
+  //append(value) adds a new node containing value to the end of the list
+  append(value) {
+    const newNode = new Node(value);
+    //if empty, make headNode
+    if (!this.headNode) {
+      this.headNode = newNode;
+    } else {
+      //else append to the end, after tailNode
+      this.tailNode.nextNode = newNode;
+    }
+    this.tailNode = newNode;
+    this._size++;
+  }
+}
