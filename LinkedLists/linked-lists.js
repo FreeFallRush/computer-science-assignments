5e7d2dd444cba1184fe3458193a7c34c32b3c84f
FreeFallRush <queenofcups.altbot@gmail.com>
2023-07-23 01:26:10 +0300

    created Node class

--- LinkedLists/linked-lists.js
+++ LinkedLists/linked-lists.js
@@ -1,1 +1,11 @@
+//Assignment:
+//You will need two classes or factories:
+// -> LinkedList class / factory, which will represent the full list.
+// -> Node class / factory, containing a value property and a link to the nextNode, set both as null by default.
 
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
