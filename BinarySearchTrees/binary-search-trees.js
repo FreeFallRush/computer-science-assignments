b27faa25fbdb825a651523f89e5a18a4e8efc3f1
FreeFallRush <queenofcups.altbot@gmail.com>
2023-07-27 22:13:21 +0300

    added the given prettyPrint function

--- BinarySearchTrees/binary-search-trees.js
+++ BinarySearchTrees/binary-search-trees.js
@@ -40,3 +40,18 @@ class Tree {
     return buildBST(0, sortedArr.length - 1);
   }
 }
+
+//Tip: If you would like to visualize your binary search tree, here is a prettyPrint() function that will console.log your tree
+//in a structured format. This function will expect to receive the root of your tree as the value for the node parameter
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
